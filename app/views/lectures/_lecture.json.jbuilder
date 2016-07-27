b51af47e6fef1b8c24c2b98bbd907ff48a3aded4
halspring <hal.webworks@gmail.com>
2016-07-28 00:01:21 +0900

    rails g scaffold Lecture name teacher_id:integer year:integer gakubu:integer gakka:integer term:integer type:integer

---
+++ app/views/lectures/_lecture.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! lecture, :id, :name, :teacher_id, :year, :gakubu, :gakka, :term, :type, :created_at, :updated_at
+json.url lecture_url(lecture, format: :json)
---
+++ db/migrate/20160727150057_create_lectures.rb
@@ -0,0 +1,15 @@
+class CreateLectures < ActiveRecord::Migration
+  def change
+    create_table :lectures do |t|
+      t.string :name
+      t.integer :teacher_id
+      t.integer :year
+      t.integer :gakubu
+      t.integer :gakka
+      t.integer :term
+      t.integer :type
+
+      t.timestamps null: false
+    end
+  end
+end
