fb0fddaa2b6ee04b5f36ab434d29a407c0c6cbd6
halspring <hal.webworks@gmail.com>
2016-08-29 01:54:40 +0900

    deviseにて新規登録時にUserモデルの項目入力を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-#  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:year])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:gakubu])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:gakka])
+  end
 
 #  protected
 #  def configure_permitted_parameters
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821132621) do
+ActiveRecord::Schema.define(version: 20160828164955) do
 
   create_table "exam_relations", force: :cascade do |t|
     t.integer  "exam_id"
